4bd81fbab9328566ae158b764b52d4df57f67863
M.vin <97456214+emeka-vin@users.noreply.github.com>
2023-10-02 00:40:23 +0100

    added more test cases

--- src/main/java/com/devmeks/pangenerator/model/request/CreatePANFromMobileBaseDto.java
+++ src/main/java/com/devmeks/pangenerator/model/request/CreatePANFromMobileBaseDto.java
@@ -4,13 +4,11 @@ package com.devmeks.pangenerator.model.request;
 import com.devmeks.pangenerator.model.BaseDto;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 @Getter
 @Setter
+@Builder
 @NoArgsConstructor
 @AllArgsConstructor
 @JsonInclude(JsonInclude.Include.NON_EMPTY)
