6ed0aa087095ceb7517bd5cea6f72ede2d7b3225
M.vin <97456214+emeka-vin@users.noreply.github.com>
2023-10-01 18:27:58 +0100

    added a ResponseDto class to allow for json response to the api caller

--- src/main/java/com/devmeks/pangenerator/model/request/CreatePANFromMobileBaseDto.java
+++ src/main/java/com/devmeks/pangenerator/model/request/CreatePANFromMobileBaseDto.java
@@ -1,16 +1,21 @@
-package com.devmeks.pangenerator.controller;
+package com.devmeks.pangenerator.model.request;
 
 
+import com.devmeks.pangenerator.model.BaseDto;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
 @JsonInclude(JsonInclude.Include.NON_EMPTY)
-public class RequestDto {
+
+public class CreatePANFromMobileBaseDto implements BaseDto {
 
     @JsonProperty("mobileNumber")
     private String mobileNumber;
