8933bbd8ba75f3b62c872d2f69ad05eb3bc1ea52
M.vin <97456214+emeka-vin@users.noreply.github.com>
2023-10-23 12:29:23 +0100

    added test containers to allow testing with an image of postgresql

--- src/test/java/com/devmeks/pangenerator/util/PanUtilsTest.java
+++ src/test/java/com/devmeks/pangenerator/util/PanUtilsTest.java
@@ -1,19 +1,27 @@
 package com.devmeks.pangenerator.util;
 
+import com.devmeks.pangenerator.container.PanRepoPostgresqlContainer;
 import com.devmeks.pangenerator.dto.response.ResponseDto;
 import com.devmeks.pangenerator.util.enums.ResponseStatus;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 import reactor.core.publisher.Mono;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 
 @SpringBootTest
+@Testcontainers
 @ActiveProfiles("test")
 class PanUtilsTest {
+
+  @Container
+  public static PostgreSQLContainer postgreSQLContainer = PanRepoPostgresqlContainer.getInstance();
 
   @Autowired
   private PanUtils panUtils;
