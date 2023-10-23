71d6da672b2d92804fa879e3fcb8661f7e711a24
M.vin <97456214+emeka-vin@users.noreply.github.com>
2023-10-23 15:24:58 +0100

    added test containers to allow testing with an image of postgresql

--- src/test/java/com/devmeks/pangenerator/controller/PanControllerTest.java
+++ src/test/java/com/devmeks/pangenerator/controller/PanControllerTest.java
@@ -1,18 +1,30 @@
 package com.devmeks.pangenerator.controller;
 
+import com.devmeks.pangenerator.container.PanRepoPostgresqlContainer;
 import com.devmeks.pangenerator.service.PanGenerator;
 import com.devmeks.pangenerator.util.PanUtils;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
 @WebMvcTest(PanController.class)
+@ActiveProfiles("test")
+@Testcontainers
 class PanControllerTest {
+  @Container
+  public static PostgreSQLContainer<PanRepoPostgresqlContainer> postgreSQLContainer
+      = PanRepoPostgresqlContainer.getInstance();
+
 
   @Autowired
   private MockMvc mockMvc;
